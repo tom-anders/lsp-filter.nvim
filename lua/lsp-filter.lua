7446210f965f490b7899d93eae916d451ca963ec
tom-anders <13141438+tom-anders@users.noreply.github.com>
2021-11-29 03:02:20 -0800

    Use table for parameters (expect method)

--- lua/lsp-filter.lua
+++ lua/lsp-filter.lua
@@ -2,18 +2,19 @@ local M = {}
 
 local last_results = {}
 
-M.filter_request = function(method, handler, filter, callback_on_discard)
+M.filter_request = function(method, opts)
     local params = vim.lsp.util.make_position_params()
 
-    handler = handler or vim.lsp.handlers[method]
+    handler = opts.handler or vim.lsp.handlers[method]
     if not handler then
         print("No default handler for method", method)
         return
     end
 
+    local filter = opts.filter
 
     vim.lsp.buf_request(0, method, params, function(err, results, ctx)
-        if not filter or err or not results or #results <= 1 then
+        if not opts.filter or err or not results or #results <= 1 then
             handler(err, results, ctx)
             return
         end
@@ -21,7 +22,7 @@ M.filter_request = function(method, handler, filter, callback_on_discard)
         local filtered_results = {}
         local discarded_results = {}
         for _, result in ipairs(results) do
-            table.insert(filter(result) and filtered_results or discarded_results, result)
+            table.insert(opts.filter(result) and filtered_results or discarded_results, result)
         end
 
         last_results = {
@@ -34,8 +35,8 @@ M.filter_request = function(method, handler, filter, callback_on_discard)
         if #filtered_results > 0 then
             handler(err, filtered_results, ctx)
 
-            if callback_on_discard and #discarded_results > 0 then 
-                callback_on_discard(discarded_results)
+            if opts.callback_on_discard and #discarded_results > 0 then 
+                opts.callback_on_discard(discarded_results)
             end
 
         -- If there aren't any results left after filtering, show the full results
